e81a2bb55802c6956aecfe88564a0d7e011948da
Chenel000 <saloiche@gmail.com>
2021-04-15 08:43:10 +0200

    Update OpenUV.lua

--- OpenUV.lua
+++ OpenUV.lua
@@ -5,7 +5,7 @@ return {
 			'at 08:00-20:00 every 15 minutes' -- This time range and steps allow to fit with 50 request per day limit
 		},
 		httpResponses = {
-			'openuv' -- must match with the parameter 'callback' used in dz.openURL({url =, method =, headers =, callback = openuv'})
+			'openuv' -- must match with the parameter 'callback' used in dz.openURL({url =, method =, headers =, callback = 'openuv'})
 		}
 	},
 	execute = function(dz, item)
